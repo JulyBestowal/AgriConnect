901d4b2e2568361183eb679c60759769ba1f4623
JulyBestowal <saurav.kumar.223958@gmail.com>
2025-05-11 11:08:59 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,31 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      
+      {
+        protocol: 'https',
+        hostname: 'api.dicebear.com',
+        port: '',
+        pathname: '/**',
+      }
+      
+    ],
+  },
+};
+
+export default nextConfig;
